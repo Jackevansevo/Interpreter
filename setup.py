80fc1a8aabd45f2adf856a5bc1a1b625540f1bcf
Jack Evans <jack@evans.gb.net>
2017-12-03 20:30:47 +0000

    Added missing dependency

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         ]
     },
     include_package_data=True,
-    install_requires=['graphviz'],
+    install_requires=['graphviz', 'attrs'],
     setup_requires=[
         'pytest-runner',
     ],
